b0f3faf963f484a77fc9d09cf9299824b1eb3cbe
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2022-06-11 06:51:59 +0800

    Minging Corp

--- src/cards/community/MiningCorp.ts
+++ src/cards/community/MiningCorp.ts
@@ -7,7 +7,7 @@ import {CardType} from '../../common/cards/CardType';
 import {CardRenderer} from '../render/CardRenderer';
 import {Resources} from '../../common/Resources';
 import {Size} from '../../common/cards/render/Size';
-
+import {digit} from '../../cards/Options';
 export class MiningCorp extends Card implements ICorporationCard {
   constructor() {
     super({
@@ -21,7 +21,7 @@ export class MiningCorp extends Card implements ICorporationCard {
         description: 'You start with 1 energy and steel production, 25 M€ and 10 steel. Draw 3 building cards.',
         renderData: CardRenderer.builder((b) => {
           b.br.br;
-          b.production((pb) => pb.energy(1).steel(1)).megacredits(25).text('10', Size.TINY).steel(1).text('3', Size.TINY).cards(1, {secondaryTag: Tags.BUILDING});
+          b.production((pb) => pb.energy(1).steel(1)).megacredits(25).steel(10, {digit}).cards(3, {secondaryTag: Tags.BUILDING, digit});
           b.corpBox('effect', (ce) => {
             ce.effect('Your steel resources are worth 1 M€ extra.', (eb) => {
               eb.steel(1).startEffect.plus(Size.SMALL).megacredits(1);
@@ -33,7 +33,7 @@ export class MiningCorp extends Card implements ICorporationCard {
   }
 
   public play(player: Player) {
-    player.addResource(Resources.STEEL, 10)
+    player.addResource(Resources.STEEL, 10);
     player.addProduction(Resources.ENERGY, 1);
     player.addProduction(Resources.STEEL, 1);
     player.drawCard(3, {tag: Tags.BUILDING});
