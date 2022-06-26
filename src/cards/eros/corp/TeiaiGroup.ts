1f691a4663b477377102896565c70871a037f454
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2022-06-27 01:32:10 +0800

    update Teiai

--- src/cards/eros/corp/TeiaiGroup.ts
+++ src/cards/eros/corp/TeiaiGroup.ts
@@ -6,25 +6,34 @@ import {CardType} from '../../../common/cards/CardType';
 import {ICorporationCard} from '../../corporation/ICorporationCard';
 import {Player} from '../../../Player';
 import {Tags} from '../../../common/cards/Tags';
+import {SelectAmount} from '../../../inputs/SelectAmount';
+import {DiscardCards} from '../../../deferredActions/DiscardCards';
+import {DrawCards} from '../../../deferredActions/DrawCards';
+import {Priority} from '../../../deferredActions/DeferredAction';
+import {Size} from '../../../common/cards/render/Size';
 
 export class TeiaiGroup extends Card implements ICard, ICorporationCard {
   constructor() {
     super({
       name: CardName.TEIAI_GROUP,
       tags: [Tags.BUILDING],
-      startingMegaCredits: 51,
+      startingMegaCredits: 47,
       cardType: CardType.CORPORATION,
 
       metadata: {
         cardNumber: 'Q30',
-        description: `You start with 51 MC.`,
+        description: '',
         renderData: CardRenderer.builder((b) => {
-          b.br.br.br.br;
-          b.megacredits(51);
+          b.br.br.br;
+          b.megacredits(47).br;
+          b.text('(You start with 47 MC.)', Size.TINY, false, false);
           b.corpBox('action', (ce) => {
-            ce.vSpace();
-            ce.action('Decrease 1 TR to draw 2 card. If you have no card, you do not need to decrease TR.', (eb) => {
-              eb.minus().tr(1).asterix().startAction.text('2').cards(1);
+            ce.vSpace(Size.LARGE);
+            ce.action('If you have no card, draw 2 card.', (eb) => {
+              eb.empty().startAction.text('2').cards(1).asterix();
+            });
+            ce.action('Or you can discard X cards to draw X-1 cards. Maximum X is current generation number.', (eb) => {
+              eb.or().text('X').cards(1).startAction.text('X-1').cards(1);
             });
           });
         }),
@@ -49,8 +58,19 @@ export class TeiaiGroup extends Card implements ICard, ICorporationCard {
     if (player.cardsInHand.length === 0) {
       player.drawCard(2);
     } else {
-      player.decreaseTerraformRatingSteps(1);
-      player.drawCard(2);
+      const max = Math.min(player.cardsInHand.length, player.game.generation);
+
+      return new SelectAmount(
+        'Select number of cards to discard',
+        'Discard cards',
+        (amount: number) => {
+          player.game.defer(new DiscardCards(player, amount), Priority.DISCARD_BEFORE_DRAW);
+          player.game.defer(DrawCards.keepAll(player, amount-1));
+          return undefined;
+        },
+        1,
+        max,
+      );
     }
     return undefined;
   }
