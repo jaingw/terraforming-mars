052fb96495a505cfd235f6962ad805733ed4d1d6
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2022-07-02 14:33:18 +0800

    fix Sith bugs

--- src/cards/eros/corp/SithOrganizations.ts
+++ src/cards/eros/corp/SithOrganizations.ts
@@ -20,18 +20,19 @@ export class SithOrganizations extends Card implements ICard, ICorporationCard {
     super({
       name: CardName.SITH_ORGANIZATIONS,
       tags: [Tags.VENUS, Tags.JOVIAN],
-      startingMegaCredits: 35,
+      startingMegaCredits: 38,
       cardType: CardType.CORPORATION,
 
       metadata: {
         cardNumber: 'Q48',
-        description: 'You start with 35 M€.',
+        // description: 'You start with 38 M€. As your first action, transform all Neutral delegates to your delegates.',
         renderData: CardRenderer.builder((b) => {
           b.br;
-          b.megacredits(35, {size: Size.TINY});
+          b.megacredits(38, {size: Size.TINY}).nbsp.nbsp.chairman().br;
+          b.text('(You start with 38 M€. As your first action, transform all Neutral delegates to your delegates.)', Size.TINY, false, false);
           b.corpBox('action', (ce) => {
             ce.vSpace(Size.LARGE);
-            ce.action('Send a Neutral delegate to any party.', (eb) => {
+            ce.action('Send a delegate to any party.', (eb) => {
               eb.empty().startAction.delegates(1).asterix();
             });
             ce.effect('Your delegates count as neutral delegates. If neutral delegate becomes chairman, you can decide: let all other player lose 1 TR, or ignore their ruling bonus.', (eb) => {
@@ -43,7 +44,11 @@ export class SithOrganizations extends Card implements ICard, ICorporationCard {
     });
   }
 
-  public play(player: Player) {
+  public play() {
+    return undefined;
+  }
+
+  public initialAction(player: Player) {
     const game = player.game;
     if (game.turmoil !== undefined) {
       const turmoil = game.turmoil;
@@ -64,6 +69,7 @@ export class SithOrganizations extends Card implements ICard, ICorporationCard {
       if (index > -1) {
         turmoil.delegateReserve.splice(index, 1);
       }
+      game.log('All Neutral delegates count as ${0}', (b) => b.player(player));
       return undefined;
     }
     return undefined;
